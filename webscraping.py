319981b4320878b1815f445005080b523f22903b
Trevor O'Hearn <trevohearn@gmail.com>
2020-05-06 18:24:32 -0400

    outlined webscraping python file

---
+++ webscraping.py
@@ -0,0 +1,23 @@
+#Trevor O'Hearn
+#5/6/2020
+#Python file for webscraping methods
+
+
+#installs
+#!pip install Selenium
+
+import Selenium as sl
+import BeautifulSoup as bs
+
+### SELENIUM METHODS ###
+
+### BEAUTIFULSOUP METHODS ###
+
+#given HTML element
+#get parent elements
+
+#return specific child elements
+
+#parse text out of given elements
+
+#change webpage to scrape
